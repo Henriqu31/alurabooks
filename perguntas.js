dd2b4b4a91b6c9043266040620ce436c97ee9287
Henriqu31 <viniciushgsilva050@gmail.com>
2024-12-04 08:09:59 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,70 @@
+criaCartao(
+    'Programação',
+    'O que é UX?',
+    'Experiencia de usuario'
+)
+
+criaCartao(
+    'Matematica',
+    '2X2 ?',
+    '4'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capital do Espirito Santo?',
+    'Vitoria'
+)
+
+criaCartao(
+    'geografia',
+    'Qual a capital de Sao Paulo?',
+    'Sao Paulo'
+)
+
+criaCartao(
+    'Historia',
+    'Quando aconteceu a sengunda guerra mundial?',
+    'no ano de 1945'
+)
+
+criaCartao(
+    'Esporte',
+    'Quem e o rei do futebol?',
+    'Pele'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capital do Rio de Janeiro?',
+    'Rio de janeiro'
+)
+
+criaCartao(
+    'Esportes',
+    'quem e o maior ganhador da bola de ouro?',
+    'Lionel Messi'
+)
+criaCartao(
+    'Matematica',
+    '5X2?',
+    '10'
+)
+
+criaCartao(
+    'Matematica',
+    '6X6?',
+    '36'
+)
+
+criaCartao(
+    'Geografia',
+    'qual é a capita do Brasil?',
+    'Brasilia'
+)
+
+criaCartao(
+    'Esportes',
+    'Quem ganhou a bola de ouro em 2024?',
+    'Rodri'
+)
